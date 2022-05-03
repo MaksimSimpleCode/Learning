a05e9bbf5deefb7161115c5ee627c2ad2f8e3b28
MaksimSimpleCode <80915302+MaksimSimpleCode@users.noreply.github.com>
2022-05-03 11:28:34 +0400

    SignalR пример

---
+++ ASP.NET Core/SignalRExample/SignalRClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static HubConnection HubConnection;
+        static async Task Main(string[] args)
+        {
+            HubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:18244/notification")
+                .Build();
+
+            HubConnection.On<string>("Send", message => Console.WriteLine($"Message From Server: {message}"));
+
+            await HubConnection.StartAsync();
+
+            bool isExit = false;
+            while (!isExit)
+            {
+                var message = Console.ReadLine();
+                if (message != "exit")
+                {
+                    await HubConnection.SendAsync("SendMessage", message);
+                }
+                else
+                    isExit = true;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ASP.NET Core/SignalRExample/SignalRExample/Hubs/NotificationHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRExample.Hubs
+{
+    public class NotificationHub:Hub
+    {
+        public Task SendMessage(string message)
+        {
+
+            return Clients.All.SendAsync("Send",message);
+
+        }
+    }
+}
