e6e2a27beacf252a124def3205d9872a6b5d5d56
MaksimSimpleCode <80915302+MaksimSimpleCode@users.noreply.github.com>
2022-06-28 00:16:15 +0400

    Урок 57

--- Frontend/React/YouTube/IT-Kamasutra/react-learn/src/components/Users/UsersContainer.jsx
+++ Frontend/React/YouTube/IT-Kamasutra/react-learn/src/components/Users/UsersContainer.jsx
@@ -1,16 +1,52 @@
 import { connect } from 'react-redux';
-import { followAC, setUsersAC, unfollowAC } from '../../redux/users-reducer';
+import { followAC, setUsersAC, unfollowAC ,setCurrentPageAC,setTotalUsersCountAC} from '../../redux/users-reducer';
+import axios from 'axios';
+import React from 'react';
 import Users from './Users';
 
 
-//Эта штука мапит свойства в  пропсы  презентационной компоненты
-let mapStateToProps = (state) => {
-    return {
-        users: state.usersPage.users
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+
+       return <Users totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+            />
     }
 }
 
-//Эта штука мапит методы в пропсы презентационной компоненты
+//Эта штука мапит свойства в  пропсы  презентационной компоненты.
+let mapStateToProps = (state) => {
+    return {
+        users: state.usersPage.users,
+        pageSize:state.usersPage.pageSize,
+        totalUsersCount:state.usersPage.totalUsersCount,
+        currentPage:state.usersPage.currentPage,
+    }
+}
+
+//Эта штука мапит методы в пропсы презентационной компоненты. А берет их из редьюсера.
 let mapDispatchToProps = (dispatch) => {
     return {
        follow:(userId)=>{
@@ -21,10 +57,17 @@ let mapDispatchToProps = (dispatch) => {
        },
        setUsers:(users)=>{
         dispatch(setUsersAC(users));
+       },
+       setCurrentPage:(pageNumber)=>{
+        dispatch(setCurrentPageAC(pageNumber));
+       },
+       setTotalUsersCount:(totalCount)=>{
+        dispatch(setTotalUsersCountAC(totalCount));
        }
+
     }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
--- Frontend/React/YouTube/IT-Kamasutra/react-learn/src/redux/users-reducer.js
+++ Frontend/React/YouTube/IT-Kamasutra/react-learn/src/redux/users-reducer.js
@@ -1,12 +1,16 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USER = "SET_USER";
+const SET_PARENT_PAGE = "SET_PARENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 
 //Initials
 let initialState = {
     users: [],
-    newPostText: "Maskim-Learn.com"
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 1
 }
 
 
@@ -34,17 +38,24 @@ const usersReducer = (state = initialState, action) => {
                 })
             }
         case SET_USER:
-            return { ...state, users: [...state.users, ...action.users] }
+            return { ...state, users: action.users }
+        case SET_PARENT_PAGE:
+            return { ...state, currentPage: action.currentPage }
+        case SET_TOTAL_USERS_COUNT:
+            return { ...state, totalUsersCount: action.count}
 
         default:
             return state;
     }
 }
 
-//Action creaters
+//Action creaters (используем их при диспатче в контейнерных компонентах)
 export const followAC = (userId) => ({ type: FOLLOW, userId });
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
 export const setUsersAC = (users) => ({ type: SET_USER, users });
+export const setCurrentPageAC = (currentPage) => ({ type: SET_PARENT_PAGE, currentPage: currentPage });
+export const setTotalUsersCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count:totalUsersCount});
+
 
 
 
