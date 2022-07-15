4a0d91e2154ad24d53bd02342aabe7e0748ba5e1
Maksim Gusev <gusev@samarasoft.com>
2022-07-15 14:39:59 +0400

    Test polymofizm

---
+++ C#/generic is polimorfizm/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            var str = "str";
+            var number = 42;
+            Generic(car);
+            Generic(str);
+            Generic(number);
+        }
+        public static void  Generic<T>(T obj)
+        {
+            if (obj is Car)
+            {
+                Console.WriteLine("тачка");
+            }
+            else if (obj is string)
+            {
+                Console.WriteLine("строка");
+            }
+            else
+                Console.WriteLine("ХЗ");
+
+        }
+    }
+    public class Car
+    {
+
+    }
+}
