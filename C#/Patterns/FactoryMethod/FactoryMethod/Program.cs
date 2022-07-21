568c414d3cc2c3b1e93729edd9a2cfa29a4d2c0d
Maksim Gusev <gusev@samarasoft.com>
2022-07-21 15:18:26 +0400

    FactoryMethod

---
+++ C#/Patterns/FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FactoryMethod.Factories;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            Console.WriteLine("Введите тип абонимента который хотите создать....");
+            Console.WriteLine("1-Базовый\n2-С бассейном\n3-С персональными тренировками");
+
+            string membershipType = Console.ReadLine();
+
+            MembershipFactory factory = GetFactory(membershipType);
+            Domain.IMembership membership = factory.GetMembership();
+
+            Console.WriteLine($"\tНазвание:\t\t{membership.Name}\n" +
+                $"\tОписание:\t{membership.Description}\n" +
+                $"\tЦена:\t\t{membership.GetPrice()}");
+
+            Console.ReadLine();
+        }
+
+        private static MembershipFactory GetFactory(string membershipType)
+        {
+          return  membershipType.ToLower() switch
+            {
+                "1" => new GymMembershipFactory(100, "Базовый абонемент"),
+                "2" => new GymPlusPoolMembershipFactory(200, "Абонемент с бассейном"),
+                "3" => new PersonalTrainingMembershipFactory(400, "Абонемент с персональными тренировками"),
+                _ => null
+            };
+        }
+        
+    }
+}
